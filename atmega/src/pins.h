823b77a159bffadb46fe1706036458ce98208d4b
WiseLord <wiselord1983@gmail.com>
2015-05-23 16:50:21 +0300

    AVR ATmega wizard added

---
+++ atmega/src/pins.h
@@ -0,0 +1,16 @@
+#ifndef PINS_H
+#define PINS_H
+
+#include <avr/io.h>
+
+#define CONCAT(x,y)			x ## y
+
+#define DDR(x)				CONCAT(DDR,x)
+#define PORT(x)				CONCAT(PORT,x)
+#define PIN(x)				CONCAT(PIN,x)
+
+
+#define LED_GREEN			C
+#define LED_GREE_LINE		(1<<0)
+
+#endif /* PINS_H */
