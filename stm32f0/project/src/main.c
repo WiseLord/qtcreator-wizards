0aacb7e70bfd381f21f91942a7b8eefc0794c041
WiseLord <wiselord1983@gmail.com>
2015-06-03 15:40:55 +0300

    Modify stm32f0 template for using USE_STDPERIPH_DRIVER macros.

--- stm32f0/project/src/main.c
+++ stm32f0/project/src/main.c
@@ -1,7 +1,4 @@
 #include <stm32f0xx.h>
-#include <stm32f0xx_gpio.h>
-
-// #include "pins.h"
 
 GPIO_InitTypeDef ledG, ledB;
 
@@ -14,15 +11,11 @@ void SysTick_Handler(void) {
 	switch (tick) {
 	case 0:
 		GPIO_ResetBits(GPIOC, ledB.GPIO_Pin);
-		// GPIO_CLR(LED_B);
 		GPIO_SetBits(GPIOC, ledG.GPIO_Pin);
-		// GPIO_SET(LED_G);
 		break;
 	case 10:
-		// GPIO_CLR(LED_C);
 		GPIO_ResetBits(GPIOC, ledG.GPIO_Pin);
 		GPIO_SetBits(GPIOC, ledB.GPIO_Pin);
-		// GPIO_SET(LED_B);
 		break;
 	}
 }
@@ -38,14 +31,12 @@ void hwInit()
 	ledB.GPIO_Speed = GPIO_Speed_2MHz;
 	ledB.GPIO_OType = GPIO_OType_PP;
 	ledB.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	// GPIO_OUT(LED_B);
 
 	ledG.GPIO_Pin = GPIO_Pin_9;
 	ledG.GPIO_Mode = GPIO_Mode_OUT;
 	ledG.GPIO_Speed = GPIO_Speed_2MHz;
 	ledG.GPIO_OType = GPIO_OType_PP;
 	ledG.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	// GPIO_OUT(LED_G);
 
 	GPIO_Init(GPIOC, &ledB);
 	GPIO_Init(GPIOC, &ledG);
